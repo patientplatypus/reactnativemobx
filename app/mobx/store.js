12da73218fda4af615828c4110778df8ea7eaa1e
Peter Weyand <patientplatypus@Peters-MacBook-Pro.local>
2017-06-17 22:01:17 -0500

    computed up

--- app/mobx/store.js
+++ app/mobx/store.js
@@ -1,5 +1,6 @@
-import { observable, action } from 'mobx';
+import { observable, action, computed } from 'mobx';
 import { observer } from 'mobx-react';
+
 
 
 class MobxStore {
@@ -8,15 +9,33 @@ class MobxStore {
   @observable counter2=10;
   @observable userName=null;
 
-  @action addOne = ()=>{
-    this.counter = this.counter+1;
-  }
-  @action subtractOne = ()=>{
-    this.counter2 = this.counter2-1;
-  }
+  @observable yearlyIncome=0;
+  @observable monthlyRent=0;
+  @observable weeklyFood=0;
+  @observable dailyOther=0;
+
+
   @action setUser = (name)=>{
     this.userName=name;
   }
+
+  @action setyearlyIncome = (number)=>{
+    this.yearlyIncome = number;
+  }
+  @action setmonthlyRent = (number)=>{
+    this.monthlyRent = number;
+  }
+  @action setweeklyFood = (number)=>{
+    this.weeklyFood = number;
+  }
+  @action setdailyOther = (number)=>{
+    this.dailyOther = number;
+  }
+
+  @computed get dailyAllowance() {
+    return  (this.yearlyIncome-12*this.monthlyRent-52*this.weeklyFood-365*this.dailyOther)/365;
+	}
+
 
 }
 
--- app/screens/TabLeft.js
+++ app/screens/TabLeft.js
@@ -14,30 +14,41 @@ import { observable, action } from 'mobx';
 import { observer } from 'mobx-react';
 import mobxStore from '../mobx/store';
 
+import renderIf from 'render-if';
+
 
 @observer
 class TabLeft extends React.Component {
-
-  addOne(){
-    mobxStore.addOne();
+  constructor(props){
+    super(props);
+    this.state={
+      yearlyIncome:0,
+      monthlyRent:0,
+      weeklyFood:0,
+      dailyOther:0,
+      allowanceComputed: false
+    }
   }
-  subtractOne(){
-    mobxStore.subtractOne();
-  }
 
-  componentWillMount(){
-    // fetch('http://localhost:3000/rtvfrmdb/', {
-    //   method: 'POST',
-    //   headers: {
-    //     'Accept': 'application/json',
-    //     'Content-Type': 'application/json',
-    //   },
-    //   body: JSON.stringify({
-    //     text:sendtext,
-    //   })
-    // })
-    // .then((response) => response.json())
-    // .done();
+  piggelfi(){
+    console.log('inside piggelfi');
+    console.log('value of this.state.yearlyIncome ', this.state.yearlyIncome);
+    console.log('value of this.state.monthlyRent ', this.state.monthlyRent);
+    console.log('value of this.state.weeklyFood ', this.state.weeklyFood);
+    console.log('value of this.state.dailyOther ', this.state.dailyOther);
+
+    mobxStore.setyearlyIncome(this.state.yearlyIncome);
+    mobxStore.setmonthlyRent(this.state.monthlyRent);
+    mobxStore.setweeklyFood(this.state.weeklyFood);
+    mobxStore.setdailyOther(this.state.dailyOther);
+    this.setState({
+      allowanceComputed: true
+    },()=>{
+      console.log('value of this.State.allowanceComputed is ', this.state.allowanceComputed);
+    });
+
+    console.log('value of mobxStore.dailyAllowance ', mobxStore.dailyAllowance);
+
   }
 
   render(){
@@ -48,20 +59,71 @@ class TabLeft extends React.Component {
         <Text></Text>
         <Text>PiggeldyProfile</Text>
         <Text>Welcome, {mobxStore.userName}</Text>
-        <Button
-          title='counterbutton'
-          onPress={this.addOne}
-        />
-        <Button
-          title='counterbutton2'
-          onPress={this.subtractOne}
-        />
-        <Text>Counter Add One Counter: {mobxStore.counter}</Text>
-        <Text>Counter Subtract One Counter: {mobxStore.counter2}</Text>
-        <Text>This is the left tab</Text>
+        <TextInput
+           style={styles.moneyInput}
+           placeholder='annual income'
+           onChangeText={(e) => this.setState({yearlyIncome:e})}
+           value={this.state.yearlyIncome}
+         />
+        <TextInput
+           style={styles.moneyInputRed}
+           placeholder='monthly rent'
+           onChangeText={(e) => this.setState({monthlyRent:e})}
+           value={this.state.monthlyRent}
+         />
+         <TextInput
+           style={styles.moneyInputRed}
+           placeholder='weekly food expenses'
+           onChangeText={(e) => this.setState({weeklyFood:e})}
+           value={this.state.weeklyFood}
+         />
+         <TextInput
+           style={styles.moneyInputRed}
+           placeholder='daily expenses'
+           onChangeText={(e) => this.setState({dailyOther:e})}
+           value={this.state.dailyOther}
+         />
+         <Button
+           title='piggelfi'
+           onPress={()=>this.piggelfi()}
+         />
+
+         {renderIf(this.state.allowanceComputed===true)(
+          <View>
+            <Text>Your daily allowance is: </Text>
+            <Text>{mobxStore.dailyAllowance}</Text>
+          </View>
+         )}
+
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  moneyInput: {
+    marginTop:5,
+    marginBottom:5,
+    height: 40,
+    borderColor: 'seagreen',
+    borderWidth: 1,
+    width: '90%',
+    marginLeft: 'auto',
+    marginRight: 'auto'
+  },
+  moneyInputRed: {
+    marginTop:5,
+    marginBottom:5,
+    height: 40,
+    borderColor: 'tomato',
+    borderWidth: 1,
+    width: '90%',
+    marginLeft: 'auto',
+    marginRight: 'auto'
+  },
+  red: {
+    color: 'red',
+  },
+});
 
 export default TabLeft
